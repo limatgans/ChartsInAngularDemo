c1fc6f8841b1806a1da15f6d6a1ed560e52adcc9
limatgans <limatgans@gmail.com>
2018-12-02 22:53:39 +0530

    Initialization of Chart Component

--- ChartsInAngularDemo/src/app/app.component.ts
+++ ChartsInAngularDemo/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ChartsInAngularDemo';
+  title = 'Charts - Angular - D3 Demo';
 }
